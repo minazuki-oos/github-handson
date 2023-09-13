cb0cd5bce0e56fa1f490f12128328fc5a8df79f9
minazuki <aratanotennis@gmail.com>
2023-09-13 19:19:16 +0900

    com.github追加

---
+++ com.github/src/ArraylistExample.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraylistExample {
+    public static void generateListAndPrint() {
+        // ① String型のListを作成
+        List<String> stringList = new ArrayList<>();
+
+        // ② Listに複数の要素を追加
+        stringList.add("Hello");
+        stringList.add("World");
+        stringList.add("こんにちは");
+        stringList.add("GPT-3.5");
+        stringList.add("ChatGPT");
+
+        // Listの内容を表示
+        System.out.println(stringList);
+    }
+
+    public static void main(String[] args) {
+        // generateListAndPrintメソッドを呼び出す
+        generateListAndPrint();
+    }
+}
